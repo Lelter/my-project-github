46f72c1394340af742df78ce38a2297871e0a00d
lelter <1067045938@qq.com>
2023-09-19 23:51:45 +0800

    feature:update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/61485693.png" style="width: 200px;height:200px;border: 1px;border-radius: 20px 20px 20px 20px">
+  <HelloWorld msg="个人介绍"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      姓名：叶宇涛
+    </p>
+    <h3>昵称</h3>
+    <ul>
+      <li>Lelter</li>
+    </ul>
+    <h3>爱好</h3>
+    <ul>
+      <li>游戏</li>
+      <li>看番</li>
+      <li>睡觉</li>
+      <li>打球</li>
+    </ul>
+   <h1 style="color:#189a18">谢谢大家！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
